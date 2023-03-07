c7fb8ca5e4a2a19ce087c4b1cb26c917f12b96ed
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-07 11:47:30 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-function add(a, b){
-    const result = a + b
-    return result;
+function add(a,b) {
+  return a + b
 }
-function subtract(a, b){
-    const result = a - b
-    return result;
+
+function subtract(a,b) {
+  return a - b
 }
-function multiply(a, b){
-    const result = a * b
-    return result;
+
+function divide(a,b) {
+  return a / b
 }
-function divide(a, b){
-    const result = a / b
-    return result;
+
+function multiply(a,b) {
+  return a * b
 }
-// parses n as an integer and returns the parsed integer
-// ✓ assumes base 10
-// ✓ returns NaN as appropriate
-function makeInt(n){
-    const parsed =parseInt(`${n}`, 10);
-    return `${parsed}`;
+
+function increment(n) {
+  return n += 1
 }
-function increment(n){
-    const nIncreament = (n+=1)
-    return nIncreament
+
+function decrement(n) {
+  return n -= 1
 }
-function decrement(n){
-    const decrement = n - 1;
-    return decrement
+
+function makeInt(string) {
+  return parseInt(string, 10)
 }
-function preserveDecimal(n){
-    return parseFloat(n)
+
+function preserveDecimal(string) {
+  return parseFloat(string)
 }
-preserveDecimal()
